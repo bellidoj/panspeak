d345b202863f7453a20743451d918fa72e64327c
javier <bellidoj@gmail.com>
2016-03-30 00:14:59 +0200

    version 0.2

--- panspeak-gtk.py
+++ panspeak-gtk.py
@@ -8,19 +8,25 @@ from contextlib import closing
 
 class myMain:
 
+    
+
     def __init__(self):
+        self.Encode = False
         self.gladefile = "panspeak.glade"
         self.builder = Gtk.Builder()
         self.builder.add_from_file(self.gladefile)
         self.handlers = {
             "on_window1_destroy": self.windowDestroy,
-            #"on_window1_delete_event": self.windowDestroy,
-            #"on_window1_destroy_event": self.windowDestroy,
+            "on_window1_destroy_event": self.windowDestroy,
             "on_menuHelpAbout_activate": self.menuAboutActivate,
             "on_menuFileOpen_activate": self.menuOpenActivate,
             "on_menuFileSave_activate": self.menuSaveActivate,
+            "on_menuFileSaveAs_activate": self.menuSaveAsActivate,
             "on_menuFileExit_activate": self.QuitActivate,
             "on_runButton_clicked": self.translate,
+            "on_menuPreferences_activate": self.showPreferencesWindow,
+            "on_preferencesCloseButton_clicked": self.closePreferencesWindow,
+            "on_menuAddMarker_activate":self.addMarker,
             }
         self.builder.connect_signals(self.handlers)
         self.window = self.builder.get_object("window1")
@@ -60,7 +66,7 @@ class myMain:
     def menuAboutActivate(self, widget):
         dialog = Gtk.AboutDialog(None, self.window, None)
         dialog.set_program_name("Panspeak")
-        dialog.set_version("0.1")
+        dialog.set_version("0.2")
         dialog.set_comments("Get text and text to speech at once")
         dialog.set_website("http://peregilllica.com") 
         dialog.set_website_label("Peregilllica.com") 
@@ -95,6 +101,10 @@ class myMain:
         filter_all = Gtk.FileFilter()
         filter_all.set_name("Todos los archivos")
         filter_all.add_pattern("*.*")
+        filter_pspk = Gtk.FileFilter()
+        filter_pspk.set_name("Todos los archivos panspeak")
+        filter_pspk.add_pattern("*.pspk")
+        dialog.add_filter(filter_pspk)
         dialog.add_filter(filter_text)
         dialog.add_filter(filter_all)
 
@@ -105,6 +115,45 @@ class myMain:
             message = "{0} is updated. Nothing done.".format(os.path.basename(self.documentName))
             print(message)
             pass
+
+    def menuSaveAsActivate(self, widget):
+        dialog = Gtk.FileChooserDialog("Save file", self.window,
+            Gtk.FileChooserAction.SAVE)
+
+        dialog.add_button("Cancelar", Gtk.ResponseType.CANCEL)
+        dialog.add_button("Guardar", Gtk.ResponseType.ACCEPT)
+
+        dialog.set_current_name(self.documentName)
+        dialog.set_do_overwrite_confirmation(True)
+        
+        self.add_filters(dialog)
+
+        response = dialog.run()
+
+        if response == Gtk.ResponseType.ACCEPT:
+            self.documentName = dialog.get_filename()
+            self.saveFile()
+            
+        elif response == Gtk.ResponseType.CANCEL:
+            pass
+            
+        dialog.destroy()
+        self.updateTitle()
+
+    def add_filters(self, dialog):
+        filter_text = Gtk.FileFilter()
+        filter_text.set_name("Text files")
+        filter_text.add_mime_type("text/plain")
+        filter_all = Gtk.FileFilter()
+        filter_all.set_name("All files")
+        filter_all.add_pattern("*.*")
+        filter_pspk = Gtk.FileFilter()
+        filter_pspk.set_name("Todos los archivos panspeak")
+        filter_pspk.add_pattern("*.pspk")
+        dialog.add_filter(filter_pspk)
+        dialog.add_filter(filter_text)
+        dialog.add_filter(filter_all)
+
 
     def QuitActivate(self, widget):
         if self.modified:
@@ -146,6 +195,8 @@ class myMain:
         message = "File saved: {0}".format(os.path.basename(self.documentName))
         print(message)
 
+
+
     def translate(self, widget):
         pattern = re.compile(r'#-(?P<text>.*?)\|(?P<speech>.*?)-#')
         start_iter = self.mybuffer.get_start_iter()
@@ -175,6 +226,47 @@ class myMain:
                     else:
                         break
                 s_file.write(line + "\n")
+        if self.Encode:
+            with closing(open(speech_file)) as encode_file:
+                self.tts(speech_file, encode_file.read())
+            self.encode(speech_file)
+
+    def showPreferencesWindow(self, widget):
+        self.preferenceswindow = self.builder.get_object("preferencesWindow")
+        self.preferenceswindow.show()
+
+    def closePreferencesWindow(self, widget):
+        if widget.get_active():
+            self.Encode = True
+            print("Encoding: " + str(self.Encode))
+        else:
+            self.Encode = False
+            print("Enconding: " + str(self.Encode)) 
+
+        self.preferenceswindow.hide()
+
+    def addMarker(self, widget):
+        marker = "#-|-#"
+        self.mybuffer.insert_at_cursor(marker)
+        cursorPosition = self.mybuffer.get_property("cursor-position")
+        myiter = self.mybuffer.get_iter_at_offset(cursorPosition -3)
+        self.mybuffer.place_cursor(myiter)
+        
+    def tts(self, filename, text):
+        fileout = filename + ".wav"
+        command = 'pico2wave|--wave={0}|--lang=es-ES|--|{1}'.format(fileout, text)
+        subprocess.call(command.split('|'), shell=False)
+        return True
+
+    def encode(self, filename):
+        filein = filename + ".wav"
+        fileout = filename + ".mp3"
+
+        command = 'lame|{0}|{1}'.format(filein, fileout)
+        
+        subprocess.call(command.split("|"), shell=False)
+
+        return True
 
 if __name__ == "__main__":
     main = myMain()
